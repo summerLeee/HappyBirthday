fe2e9ff6834a9f5980463de8db5a071f38d4f731
summerxiumin <1679451717@qq.com>
2017-10-20 10:39:15 +0800

    Add files via upload

---
+++ little-gift/assets/js/script.js
@@ -0,0 +1,14 @@
+
+	// Configure the Spectragram plugin. Follow the instructions
+	// in the tutorial on how to generate an access token
+	jQuery.fn.spectragram.accessData = {
+		accessToken: 'YOUR-ACCESS-TOKEN',
+		clientID: 'CLIENT-ID'
+	};
+	
+	
+	
+	
+
+	
+
